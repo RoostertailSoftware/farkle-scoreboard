a318eb44fc12c21bb61c3e92e519096bff717d39
John R. Brewster <John@BrewsterFamily.us>
2021-11-27 08:42:02 -0500

    Added roll buttons, dice for game play score

--- src/main/javascript/farkle-ui/src/app/components/roll-action-buttons/roll-action-buttons.component.ts
+++ src/main/javascript/farkle-ui/src/app/components/roll-action-buttons/roll-action-buttons.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PlayersService } from '@services';
+import { PlayerClass } from "@classes";
+
 @Component({
   selector: 'app-roll-action-buttons',
   templateUrl: './roll-action-buttons.component.html',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RollActionButtonsComponent implements OnInit {
 
-  constructor() { }
+  diceCount: number;
+
+  playersObservable: any;
+  constructor( private playerSvc: PlayersService ) { 
+    this.reset();
+    this.playersObservable = this.playerSvc.getObservableData();
+    this.playersObservable.subscribe( (result: Array< PlayerClass >) =>{
+      let x:PlayerClass  = result[0];
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  roll(){
+
+  }
+
+  farkle(){
+    this.reset();
+  }
+  finished(){
+    this.reset();
+  }
+
+  one(){
+    this.decrement();
+  }
+  two(){
+    this.decrement();
+  }
+  three(){
+    this.decrement();
+  }
+  four(){
+    this.decrement();
+  }
+  five(){
+    this.decrement();
+  }
+  six(){
+    this.decrement();
+  }
+
+  decrement(){
+    this.diceCount -= 1;
+  };
+  reset(){
+    this.diceCount = 6;
+  }
 }
