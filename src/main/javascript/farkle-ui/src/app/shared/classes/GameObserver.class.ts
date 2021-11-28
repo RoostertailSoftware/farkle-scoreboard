0ba5b4ee4159e0356fdf5b81a79607c998920404
John R. Brewster <John@BrewsterFamily.us>
2021-11-27 22:44:00 -0500

    an overall gameing class

---
+++ src/main/javascript/farkle-ui/src/app/shared/classes/GameObserver.class.ts
@@ -0,0 +1,24 @@
+
+import { PlayerClass, RulesConfigurationClass  } from "@classes";
+import { ConfigurationService } from "@services";
+
+export class GameObserverClass {
+
+    gameConfig: RulesConfigurationClass;
+
+    player: PlayerClass;
+    onDeck: PlayerClass;
+
+    roll: number;
+    score: number;
+
+    configObserver: any;
+    constructor( private configSvc: ConfigurationService ){
+        this.configObserver = this.configSvc.getObservableData();
+        this.configObserver.subscribe( (result: RulesConfigurationClass ) => {
+            this.gameConfig = result;
+        });
+    }
+
+    
+}
