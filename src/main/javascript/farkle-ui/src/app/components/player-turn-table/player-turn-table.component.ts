2e06f1bcdd995b3fd577ad2b76c194f73040e466
John R. Brewster <John@BrewsterFamily.us>
2021-12-31 12:38:44 -0500

    modified the table for sorting reverse, shows farkle etc.

--- src/main/javascript/farkle-ui/src/app/components/player-turn-table/player-turn-table.component.ts
+++ src/main/javascript/farkle-ui/src/app/components/player-turn-table/player-turn-table.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, ViewChild  } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort, Sort } from '@angular/material/sort';
 
 
 import { PlayersService } from '@/app/shared/services';
@@ -12,7 +13,9 @@ import * as _ from 'lodash';
   templateUrl: './player-turn-table.component.html',
   styleUrls: ['./player-turn-table.component.scss']
 })
-export class PlayerTurnTableComponent implements OnInit {
+export class PlayerTurnTableComponent {
+
+  @ViewChild( MatSort, { static: true } ) sort: MatSort;
 
   playerDataSource: MatTableDataSource< TurnClass >;
   displayedColumns: Array< string >;
@@ -27,15 +30,24 @@ export class PlayerTurnTableComponent implements OnInit {
 
   private changeData = ( result: Array< PlayerClass > ): void => {
     this.playerDataSource = new MatTableDataSource < TurnClass>( [] );
-
+    
     let i = _.findIndex( result, { active: true });
     if( _.gte( i, 0 ) ) {
       this.playerDataSource = new MatTableDataSource< TurnClass> ( result[ i ].game.turn );
+
+      this.playerDataSource.sort = this.sort;
+      const sortState: Sort = { active: "turn", direction: "desc" };
+      this.sort.active = sortState.active;
+      this.sort.direction = sortState.direction;
     }
   };
-
-  ngOnInit(): void {
-    
-  }
+  
+  public getScore( playerTurn: TurnClass ):number|string {
+    let result: number|string = playerTurn.score;
+    if ( _.eq( result , 0 ) && !_.eq( this.playerDataSource.data.length, playerTurn.turn ) ){
+      result = "Farkle!";
+    };
+    return result;
+  };
 
 }
