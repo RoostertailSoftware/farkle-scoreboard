0d5925ac2b23884281417becca5737e2d6380045
John R. Brewster <John@BrewsterFamily.us>
2021-12-30 21:53:39 -0500

    Added new spaces for turn/roll data

---
+++ src/main/javascript/farkle-ui/src/app/components/player-roll-table/player-roll-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-roll-table',
+  templateUrl: './player-roll-table.component.html',
+  styleUrls: ['./player-roll-table.component.scss']
+})
+export class PlayerRollTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/main/javascript/farkle-ui/src/app/components/player-turn-table/player-turn-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-turn-table',
+  templateUrl: './player-turn-table.component.html',
+  styleUrls: ['./player-turn-table.component.scss']
+})
+export class PlayerTurnTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
