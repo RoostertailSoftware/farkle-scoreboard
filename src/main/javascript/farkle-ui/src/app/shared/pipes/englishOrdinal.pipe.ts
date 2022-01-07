901bb1fd1082cb2ae6ecb278987050f23c9490bf
John R. Brewster <John@BrewsterFamily.us>
2022-01-07 11:34:32 -0500

    fixed an error in logic/calculations

--- src/main/javascript/farkle-ui/src/app/shared/pipes/englishOrdinal.pipe.ts
+++ src/main/javascript/farkle-ui/src/app/shared/pipes/englishOrdinal.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as _ from 'lodash';
 
 @Pipe({
   name: 'englishOrdinal'
@@ -12,22 +13,32 @@ export class englishOrdinalPipe implements PipeTransform {
     // return the number with the ordinal
 
     let stringNumber: string = ""+value;
-    let lastCharOfStringNumber: string =  stringNumber.substring( stringNumber.length - 1 );
-
     let ordinal: string = "th";
-    switch( lastCharOfStringNumber ){
-      case "1":
-        ordinal = "st";
-        break;
-      case "2":
-        ordinal = "nd";
-        break;
-      case "3":
-        ordinal = "rd";
-        break;
+    // The teens really screw things up.  But, 4 .. 20 are all `th` so,
+    // lets start with  eliminating those easy ones first, then
+    // check the last number for the remaining numbers.  I have not
+    // gotten into anything > 99 though
+    if( _.gte( value, 4 ) && _.lte( value, 20 ) ) {
+      ordinal = "th";
+    } else {
+      let lastCharOfStringNumber: string =  stringNumber.substring( stringNumber.length - 1 );
+      switch( lastCharOfStringNumber ){
+        case "1":
+          ordinal = "st";
+          break;
+        case "2":
+          ordinal = "nd";
+          break;
+        case "3":
+          ordinal = "rd";
+          break;
+        default:
+          ordinal = "th";
+          break;
+      };
+  
     };
-
     return value+""+ordinal;
-  }
+  };
 
-}
+};
