c083e1e0cc577708e54b7b9f11cfff6e8c123ae8
John R. Brewster <John@BrewsterFamily.us>
2022-01-07 23:07:46 -0500

    Changed the component to take input

--- src/main/javascript/farkle-ui/src/app/components/player-roll-table/player-roll-table.component.ts
+++ src/main/javascript/farkle-ui/src/app/components/player-roll-table/player-roll-table.component.ts
@@ -1,12 +1,12 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild, OnChanges, SimpleChanges } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort, Sort } from '@angular/material/sort';
 
 
-import { PlayersService } from '@/app/shared/services';
-import { DiceClass, PlayerClass, RollClass } from '@/app/shared/classes';
+import { DiceClass, RollClass, TurnClass } from '@/app/shared/classes';
 
 import * as _ from 'lodash';
+
 const sortState: Sort = { active: "roll", direction: "desc" };
 
 @Component({
@@ -14,45 +14,34 @@ const sortState: Sort = { active: "roll", direction: "desc" };
   templateUrl: './player-roll-table.component.html',
   styleUrls: ['./player-roll-table.component.scss']
 })
-export class PlayerRollTableComponent  {
+export class PlayerRollTableComponent implements OnChanges {
 
   @ViewChild( MatSort, { static: true } ) sort: MatSort;
+  @Input() selectedTurn: TurnClass;
 
   playerDataSource: MatTableDataSource< RollClass >;
   displayedColumns: Array< string >;
-  playerSvcObserver: any;
 
-  constructor( private playerSvc: PlayersService ) { 
+  constructor(  ) { 
     this.displayedColumns = [ 'roll', 'dice', 'score' ];
+  }
 
-    this.playerSvcObserver = this.playerSvc.getObservableData();
-    this.playerSvcObserver.subscribe( this.changeData );
-  };
-
-  // used by the View to see if this is the current row we are 
-  // on -- changes color ... purdy!
-  public currentRollRow( row: any ): boolean {
-    return _.eq( row.roll, this.playerDataSource.data.length );
-  };
-
-  // Data on the player has changed.
-  // Grab only the active player,
-  // the only the last turn in the array of turns
-  private changeData = ( result: Array< PlayerClass > ): void => {
-    this.playerDataSource = new MatTableDataSource < RollClass >( [] );
-    
-    let index = _.findIndex( result, { active: true });
-    if( _.gte( index, 0 ) ) {
-      const len = result[ index ].game.turn.length;
-      if( _.gt( len, 0 ) ) {
-        this.playerDataSource = new MatTableDataSource< RollClass > ( result[ index ].game.turn[ len -1 ].roll );
-      }
-      this.playerDataSource.sort = this.sort;
-      this.sort.active = sortState.active;
-      this.sort.direction = sortState.direction;
+  // accept a change in active Turn or selected Turn from
+  // player-turn-table.component
+  ngOnChanges( changes: SimpleChanges ): void {
+    if( !_.isUndefined( changes['selectedTurn'].currentValue ) ){
+      this.showThisOne ( changes['selectedTurn'].currentValue );
     };
   };
-  
+
+  // set the datasource to the active/selected Turn
+  private showThisOne( turn: TurnClass ) {
+    this.playerDataSource = new MatTableDataSource< RollClass >( turn.roll );
+    this.sort.active = sortState.active;
+    this.sort.direction = sortState.direction;
+    this.playerDataSource.sort = this.sort;
+  };
+
   // this is going into the table in the `dice` column.
   // currently only showing a string of the dice selected.
   // What I would like is svg of the dice(1..6) that are clickable
